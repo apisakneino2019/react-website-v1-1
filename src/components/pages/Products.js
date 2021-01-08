467521409d1c1d8ed1b0b4ab5e95999bf8940ee6
apisakneino2019 <apisak.neino@gmail.com>
2021-01-08 16:53:07 +0700

    17.00 friday

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -10,116 +10,116 @@ export default function Products() {
         <div className="item-container">
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="1" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="2" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="3" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="4" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
 
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="5" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="6" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="7" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="8" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="9" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="10" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="11" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="12" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="13" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="14" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="15" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="16" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="17" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="18" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="19" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="20" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="21" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="22" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="23" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="24" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="25" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="26" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="27" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <div className="img">
             <img src="images-notifier\coming-soon-1.jpg" alt="28" />
-            <span>ระบบจ่ายเงินเดือน</span>
+            <span>ระบบป้องกันอัคคีภัย</span>
           </div>
           <ScrollToTop showUnder={160}>
             <span>
